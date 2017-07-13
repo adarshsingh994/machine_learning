ad44403f313c2fe88a300dc91294a862f436c545
Adarsh Singh <adarshsingh994@gmail.com>
2017-07-14 00:54:49 +0530

    N Nearest Neighbours algorithm testing

--- algorithms/k_nearest_neighbour.py
+++ algorithms/k_nearest_neighbour.py
@@ -1,10 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
+import random
+import pandas as pd
 import warnings
 from math import sqrt
 from collections import Counter
-style.use('fivethirtyeight')
+
 
 def k_nearest_neighbour(data, predict, k=3):
     if len(data) >= k:
@@ -27,12 +26,31 @@ def k_nearest_neighbour(data, predict, k=3):
             vote_result = Counter(vote).most_common(1)[0][0]
     return vote_result
 
-dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
-new_features = [5, 7]
+df = pd.read_csv('../data/breast-cancer-wisconsin.data.txt', error_bad_lines=False)
+df.replace('?',-99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
 
-[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1], s=100)
-result = k_nearest_neighbour(dataset, new_features)
-plt.scatter(new_features[0], new_features[1], s=100, color=result)
-print(result)
-plt.show()
+random.shuffle(full_data)
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbour(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total)
