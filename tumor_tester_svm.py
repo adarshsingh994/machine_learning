048300ac997d834f233a16841974bb8177194c36
Adarsh Singh <adarshsingh994@gmail.com>
2017-07-15 03:19:29 +0530

    Trained the machine for tumor using svc classifier, custom svm algorithm

--- tumor_tester_svm.py
+++ tumor_tester_svm.py
@@ -1,11 +1,10 @@
 import numpy as np
-from builtins import print
-from sklearn import model_selection
 import pandas as pd
+from sklearn import model_selection, svm
 import pickle
 
 df = pd.read_csv('data/breast-cancer-wisconsin.data.txt', error_bad_lines=False)
-df.replace('?', 1, inplace=True)
+df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
 
 X = np.array(df.drop(['class'], 1))
@@ -16,16 +15,16 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 pickle_in = open('trained_classifier/trained_tumor_classifier.pickle', 'rb')
 clf = pickle.load(pickle_in)
 
-# clf = neighbors.KNeighborsClassifier()
+# clf = svm.SVC()
 # clf.fit(X_train, y_train)
-
-# with open('trained_classifier/trained_tumor_classifier.pickle', 'wb') as f:
+# confidence = clf.score(X_test, y_test)
+# print(confidence)
+#
+# with open('trained_classifier/trained_tumor_svc_classifier.pickle', 'wb') as f:
 #     pickle.dump(clf, f)
 
-confidence = clf.score(X_test, y_test)
-print(confidence)
+example_measures = np.array([[4, 2, 1, 1,1 ,2, 3, 2, 1]])
+example_measures = example_measures.reshape(len(example_measures), - 1)
 
-example_measures = np.array([[8, 7, 5, 10, 7, 9, 5, 5, 4]])
-example_measures = example_measures.reshape(len(example_measures), -1)
-forecast_set = clf.predict(example_measures)
-print(forecast_set)
+prediction = clf.predict(example_measures)
+print(prediction)
